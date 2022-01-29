fef98d8d15f442b21ef04ff15a860b156f213b36
avc0021 <avc0021@gmail.com>
2022-01-28 22:20:20 -0600

    tables generate no issues

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS department;
-DROP TABLE IF EXISTS job;
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS job;
+
 
 CREATE TABLE department (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -12,15 +13,15 @@ CREATE TABLE job (
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
   department_id INTEGER,
-  CONSTRAINT fk_party FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id INTEGER NOT NULL,
+  job_id INTEGER NOT NULL,
   manager_id INTEGER NOT NULL,
-  CONSTRAINT fk_party FOREIGN KEY (job_id) REFERENCES job(id) ON DELETE SET NULL,
-  CONSTRAINT fk_party FOREIGN KEY (manager_id) REFERENCES managers(id) ON DELETE SET NULL
+  FOREIGN KEY (job_id) REFERENCES job(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
